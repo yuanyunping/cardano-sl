cbd01c8636861089fe6bcf1e969721f98f66c16e
Moritz Angermann <moritz.angermann@gmail.com>
2018-09-20 08:41:31 +0800

    add missing colon

--- nix/overlays/faster-build.nix
+++ nix/overlays/faster-build.nix
@@ -1,4 +1,4 @@
-pkgs: localLib: self: super {
+pkgs: localLib: self: super: {
     mkDerivation = args: super.mkDerivation (args // optionalAttrs (localLib.isCardanoSL args.pname) {
       configureFlags = (args.configureFlags or []) ++ [ "--ghc-options=-O0" ];
     });
